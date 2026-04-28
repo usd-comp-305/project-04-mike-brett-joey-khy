051cfa328b1a86179d17c51d571e4123e4986221
Brett Lamey <blamey@sandiego.edu>
2026-04-28 15:39:10 -0700

    Feat: Created the Roulette class and wrote the test stubs

--- src/main/java/edu/sandiego/comp305/Roulette.java
+++ src/main/java/edu/sandiego/comp305/Roulette.java
@@ -2,4 +2,20 @@ package edu.sandiego.comp305;
 
 public class Roulette {
 
+    public int handleBet(int amount) {
+        return 0;
+    }
+
+    public int playGame() {
+        return 0;
+    }
+
+    public void updateBalance(int amount) {
+
+    }
+
+    public int spinWheel() {
+        return 0;
+    }
+
 }
