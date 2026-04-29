3ccf22c9b19de783e7e366fc4ea507a2670a0d65
Brett Lamey <blamey@sandiego.edu>
2026-04-28 20:54:39 -0700

    Feat: Created the PaiGow class and PaiGowTest class with method stubs

---
+++ src/main/java/edu/sandiego/comp305/PaiGow.java
@@ -0,0 +1,21 @@
+package edu.sandiego.comp305;
+
+public class PaiGow implements Game {
+
+    private DeckOfCards deck;
+
+    @Override
+    public int handleBet(int amount) {
+        return 0;
+    }
+
+    @Override
+    public void playGame() {
+
+    }
+
+    @Override
+    public void updateBalance(int amount) {
+
+    }
+}
---
+++ src/test/java/edu/sandiego/comp305/PaiGowTest.java
@@ -0,0 +1,21 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+public class PaiGowTest {
+
+    @Test
+    void testHandleBet() {
+
+    }
+
+    @Test
+    void testPlayGame() {
+
+    }
+
+    @Test
+    void testUpdateBalance() {
+
+    }
+}
