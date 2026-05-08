88002b84134dc34acf5ff54b09bbacbc76abc88c
Mike Roni <mroni@sandiego.edu>
2026-05-07 17:51:36 -0700

    Implemented testing and logic for basic card values and dealing

--- src/main/java/edu/sandiego/comp305/Baccarat.java
+++ src/main/java/edu/sandiego/comp305/Baccarat.java
@@ -7,7 +7,10 @@ public class Baccarat implements Game{
 
     public Baccarat(){}
 
-    public void dealInitialCards(){}
+    public void dealInitialCards(){
+        playerTotal = (getCardValue(deck.dealCard()) + getCardValue(deck.dealCard())) % 10;
+        bankerTotal = (getCardValue(deck.dealCard()) + getCardValue(deck.dealCard())) % 10;
+    }
 
     public void drawPlayerThirdCard(){}
 
@@ -17,16 +20,30 @@ public class Baccarat implements Game{
         return "";
     }
 
-    public int getCardValue(){
-        return 0;
+    public static int getCardValue(Card card){
+        String facevalue = card.getFaceValue();
+        switch (facevalue){
+            case "A": return 1;
+            case "K": case "Q": case "J": case "10": return 0;
+            default: return Integer.parseInt(facevalue);
+        }
+    }
+
+    public boolean isNatural(){
+        return (playerTotal>=8 || bankerTotal >= 8);
     }
 
     @Override
-    public void handleBet(int amount){}
+    public int handleBet(int amount){ return 0; }
 
     @Override
     public void playGame(){}
 
     @Override
     public void updateBalance(int amount){}
+
+    public int getPlayerTotal(){return playerTotal;}
+    public int getBankerTotal(){return bankerTotal;}
+    public void setPlayerTotal(int playerTotal){this.playerTotal=playerTotal;}
+    public void setBankerTotal(int bankerTotal){this.bankerTotal=bankerTotal;}
 }
--- src/test/java/edu/sandiego/comp305/BaccaratTest.java
+++ src/test/java/edu/sandiego/comp305/BaccaratTest.java
@@ -1,8 +1,86 @@
 package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaccaratTest {
+
+    private Baccarat baccarat;
+
+    @BeforeEach
+    void setUp(){baccarat = new Baccarat();}
+
+
+
+    @Test
+    void CardWorthZeroTest() {
+        Card king = new Card(Suit.SPADE, "K");
+        Card queen = new Card(Suit.SPADE, "Q");
+        Card jack = new Card(Suit.SPADE, "J");
+        Card ten = new Card(Suit.SPADE, "10");
+        assertEquals(0, baccarat.getCardValue(king));
+        assertEquals(0, baccarat.getCardValue(queen));
+        assertEquals(0, baccarat.getCardValue(jack));
+        assertEquals(0, baccarat.getCardValue(ten));
+    }
+
+    @Test
+    void AceWorthOneTest(){
+        Card ace = new Card(Suit.SPADE,"A");
+        assertEquals(1, baccarat.getCardValue(ace));
+    }
+
+    @Test
+    void CardWorthNumberNineTest(){
+        Card nine = new Card(Suit.SPADE,"9");
+        assertEquals(9, baccarat.getCardValue(nine));
+    }
+
+    @Test
+    void CardWorthNumberTwoTest(){
+        Card two = new Card(Suit.SPADE,"2");
+        assertEquals(2, baccarat.getCardValue(two));
+    }
+
+    @Test
+    void DealInitialCardWithCorrectScoringPlayerTest(){
+        baccarat.dealInitialCards();
+        assertTrue(baccarat.getPlayerTotal() >= 0 && baccarat.getPlayerTotal() <= 9);
+    }
+
+    @Test
+    void DealInitialCardWithCorrectScoringBankingTest(){
+        baccarat.dealInitialCards();
+        assertTrue(baccarat.getBankerTotal() >= 0 && baccarat.getBankerTotal() <= 9);
+    }
+
+    @Test
+    void PlayerHasNaturalNineTest(){
+        baccarat.setPlayerTotal(9);
+        baccarat.setBankerTotal(2);
+        assertTrue(baccarat.isNatural());
+    }
+
+    @Test
+    void PlayerHasNaturalEightTest(){
+        baccarat.setPlayerTotal(9);
+        baccarat.setBankerTotal(2);
+        assertTrue(baccarat.isNatural());
+    }
+
+    @Test
+    void BankerHasNaturalNineTest(){
+        baccarat.setPlayerTotal(3);
+        baccarat.setBankerTotal(9);
+        assertTrue(baccarat.isNatural());
+    }
+
+    @Test
+    void BankerHasNaturalEightTest(){
+        baccarat.setPlayerTotal(3);
+        baccarat.setBankerTotal(8);
+        assertTrue(baccarat.isNatural());
+    }
+
 }
