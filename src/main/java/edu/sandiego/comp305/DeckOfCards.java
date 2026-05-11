92bed76283182d8e33a899d1b5a35dc188604669
joeym <joeymats4@icloud.com>
2026-05-10 18:57:05 -0700

    implemented createDeckOfCards by using an embedded for loop to go through all suits and cardValues to create a full deck of cards

--- src/main/java/edu/sandiego/comp305/DeckOfCards.java
+++ src/main/java/edu/sandiego/comp305/DeckOfCards.java
@@ -1,14 +1,26 @@
 package edu.sandiego.comp305;
+import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DeckOfCards {
     public List<Card> deck;
 
-    List<Card> createDeckOfCards(){
-        return null;
+    public static ArrayList<Card> createDeckOfCards(){
+        ArrayList<Card> deck = new ArrayList<>();
+        for(Suit suit : Suit.values()){
+            for(CardValues cardValue : CardValues.values()){
+                Card card = new Card(suit, cardValue);
+                deck.add(card);
+            }
+        }
+        return deck;
     }
 
-    void shuffle(){}
+    private ArrayList<Card> shuffle(ArrayList<Card> deck){
+        return deck;
+    }
 
     public Card dealCard(){
         return null;
--- src/test/java/edu/sandiego/comp305/TestDeckOfCards.java
+++ src/test/java/edu/sandiego/comp305/TestDeckOfCards.java
@@ -1,4 +1,15 @@
 package edu.sandiego.comp305;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDeckOfCards {
+    @Test
+    void testCreatesDeckWith52Cards(){
+        ArrayList<Card> deck = DeckOfCards.createDeckOfCards();
+        assertEquals(52, deck.size());
+
+    }
 }
