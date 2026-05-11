d825f0afc4293c0c9412fd57caaf71673271e1f9
joeym <joeymats4@icloud.com>
2026-05-10 19:24:47 -0700

    created a new test to make sure that the first card is the ace of clubs

--- src/test/java/edu/sandiego/comp305/TestDeckOfCards.java
+++ src/test/java/edu/sandiego/comp305/TestDeckOfCards.java
@@ -1,15 +1,33 @@
 package edu.sandiego.comp305;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDeckOfCards {
+
+    public ArrayList<Card> deck;
+
+
+    @BeforeEach
+    void setUp() {
+        deck = DeckOfCards.createDeckOfCards();
+    }
+
     @Test
     void testCreatesDeckWith52Cards(){
-        ArrayList<Card> deck = DeckOfCards.createDeckOfCards();
         assertEquals(52, deck.size());
 
     }
+
+    @Test
+    void testFistCardIsAceOfClubs(){
+        assertEquals(CardValues.ACE, deck.getFirst().cardValue);
+        assertEquals(Suit.CLUB, deck.getFirst().suit);
+        }
+
+
+
 }
