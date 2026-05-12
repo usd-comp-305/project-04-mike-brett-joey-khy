a94b3d3cef63d04c524bbc8ad970efeb68e216f1
kparadis1 <kparadis@sandiego.edu>
2026-05-11 19:48:16 -0700

    test: Add stubs for GameFactory and implemented GameFactoryTest

---
+++ src/main/java/edu/sandiego/comp305/GameFactory.java
@@ -0,0 +1,8 @@
+package edu.sandiego.comp305;
+
+public class GameFactory {
+
+    public static Game getGame(int choice) {
+        return null;
+    }
+}
---
+++ src/test/java/edu/sandiego/comp305/GameFactoryTest.java
@@ -0,0 +1,45 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameFactoryTest {
+
+    @Test
+    void choice1ReturnsSlots() {
+        assertInstanceOf(Slots.class, GameFactory.getGame(1));
+    }
+
+    @Test
+    void choice2ReturnsCraps() {
+        assertInstanceOf(Craps.class, GameFactory.getGame(2));
+    }
+
+    @Test
+    void choice3ReturnsBlackjack() {
+        assertInstanceOf(Blackjack.class, GameFactory.getGame(3));
+    }
+
+    @Test
+    void choice4ReturnsBaccarat() {
+        assertInstanceOf(Baccarat.class, GameFactory.getGame(4));
+    }
+
+    @Test
+    void choice5ReturnsRoulette() {
+        assertInstanceOf(Roulette.class, GameFactory.getGame(5));
+    }
+
+    @Test
+    void invalidChoiceReturnsNull() {
+        assertNull(GameFactory.getGame(99));
+    }
+
+    @Test
+    void allReturnedGamesImplementGameInterface() {
+        for (int i = 1; i <= 5; i++) {
+            assertInstanceOf(Game.class, GameFactory.getGame(i));
+        }
+    }
+}
