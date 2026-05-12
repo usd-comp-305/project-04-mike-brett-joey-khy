370727eed88cf21e71234d743b9ab87f085425b7
joeym <joeymats4@icloud.com>
2026-05-11 17:17:32 -0700

    feat: added needed fields such as playerHand and dealerHand, and scanner. Began to implement playGame, created dealStartingHand. created tests to make sure both dealer and player are dealt two cards initially

--- src/main/java/edu/sandiego/comp305/Blackjack.java
+++ src/main/java/edu/sandiego/comp305/Blackjack.java
@@ -1,19 +1,70 @@
 package edu.sandiego.comp305;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Blackjack implements Game{
+public class Blackjack implements Game {
     private int playerTotal;
-    private  int dealerTotal;
+    private int dealerTotal;
     private int betAmount;
+    private ArrayList<Card> playerHand = new ArrayList<>();
+    private ArrayList<Card> dealerHand = new ArrayList<>();
+    private Card dealerFaceUpCard;
+    private ArrayList<Card> deck;
+    private Scanner scanner;
 
-    private void hit(){}
+    public Blackjack(Scanner scanner){
+        this.scanner = scanner;
+    }
 
-    private void split(){}
 
-    private void doubleDown(){}
+    private void hit() {
+    }
 
-    private void handleBet(int bet){}
+    private void split() {
+    }
 
-    private void playGame(){}
+    private void doubleDown() {
+    }
 
-    private void updateBalance(){}
+    public int handleBet(int bet) {
+        return 1;
+    }
+
+    public void playGame() {
+        deck = DeckOfCards.createDeckOfCards();
+        DeckOfCards.shuffleDeck(deck);
+        System.out.println("Welcome to Blackjack!!! Below are the table specific rules:\n " +
+                "1. Dealer stands on soft 17. \n" +
+                "2. You can only split once per deal. \n" +
+                "3. Blackjack pays out 3:2 \n" +
+                "How much would you like to bet:");
+
+        betAmount = scanner.nextInt();
+
+        dealStartingHand();
+
+    }
+
+    @Override
+    public void updateBalance(int amountWonOrLost) {
+    }
+
+    void dealStartingHand(){
+        playerHand.add(DeckOfCards.dealCard(deck));
+
+        dealerFaceUpCard = DeckOfCards.dealCard(deck);
+        dealerHand.add(dealerFaceUpCard);
+
+        playerHand.add(DeckOfCards.dealCard(deck));
+
+        dealerHand.add(DeckOfCards.dealCard(deck));
+
+    }
+
+    public ArrayList<Card> getPlayerHand(){
+        return playerHand;
+    }
+    public ArrayList<Card> getDealerHand() {
+        return dealerHand;
+    }
 }
--- src/test/java/edu/sandiego/comp305/TestBlackjack.java
+++ src/test/java/edu/sandiego/comp305/TestBlackjack.java
@@ -1,4 +1,26 @@
 package edu.sandiego.comp305;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TestBlackjack {
+    @Test
+    void testPlayerStartsWithTwoCards(){
+        Scanner mockScanner = mock(Scanner.class);
+        Blackjack blackjack = new Blackjack(mockScanner);
+        blackjack.playGame();
+        assertEquals(2, blackjack.getPlayerHand().size());
+    }
+
+    @Test
+    void testDealerStartsWithTwoCards(){
+        Scanner mockScanner = mock(Scanner.class);
+        Blackjack blackjack = new Blackjack(mockScanner);
+        blackjack.playGame();
+        assertEquals(2, blackjack.getDealerHand().size());
+    }
 }
